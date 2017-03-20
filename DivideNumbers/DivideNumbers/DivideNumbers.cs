ffa9e997a25c7961f59f5710050f9217c3f33136
RDaberISI <r.dabrowskitech@gmail.com>
2017-03-20 19:02:44 +0100

    Zadanie 1

---
+++ DivideNumbers/DivideNumbers/DivideNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideNumbers
+{
+    public class DivideNumbers
+    {
+        public Double Podziel(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ DivideNumbers/DivideNumbersTests/DivideNumbersTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+//chujowe, do poprawy
+namespace DivideNumbersTests
+{
+    [TestFixture]
+    public class DivideNumbersTests
+    {
+        [Test]
+        public void Podziel_DivideTwoPositiveNumbers_Calculated()
+        {
+            var calc = new DivideNumbers.DivideNumbers();            
+            double iloraz = calc.Podziel(2, 2);
+            Assert.AreEqual(1, iloraz);
+        }
+        
+        [Test]
+        public void Podziel_OneNegativeAndOnePositiveNumbers_Calculated()
+        {
+            var calc = new DivideNumbers.DivideNumbers();
+            double iloraz = calc.Podziel(-2, 2);
+            Assert.AreEqual(-1, iloraz);
+        }
+
+        [Test]
+        public void Podziel_OnePositiveAndOneNegativeNumbers_Calculated()
+        {
+            var calc = new DivideNumbers.DivideNumbers();
+            double iloraz = calc.Podziel(2, -2);
+            Assert.AreEqual(-1, iloraz);
+        }
+
+        [Test]
+        public void Podziel_DivideTwoNegativeNumbers_Calculated()
+        {
+            var calc = new DivideNumbers.DivideNumbers();
+            double iloraz = calc.Podziel(-2, -2);
+            Assert.AreEqual(1, iloraz);
+        }
+    }
+}
