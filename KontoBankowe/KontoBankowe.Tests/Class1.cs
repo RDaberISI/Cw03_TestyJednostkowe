4a0e2ca773aba3c39ed6099084fc5b474e6855f1
RDaberISI <r.dabrowskitech@gmail.com>
2017-03-20 19:04:54 +0100

    Zadanie 2

---
+++ KontoBankowe/KontoBankowe.Tests/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace KontoBankowe.Tests
+{
+    [TestFixture]
+    public class KontoBankowe
+    {
+        [Test]
+        public void WplacPieniadze_DepositeMoney_Calculated()
+        {
+            var konto1 = new Bank.KontoBankowe(200);
+            konto1.WplacPieniadze(100);
+            Assert.AreEqual(300, konto1.SprawdzStanKonta);
+        }
+
+        [Test]
+        public void WyplacPieniadze_WithdrawMoney_Calculated()
+        {
+            var konto1 = new Bank.KontoBankowe(200);
+            konto1.WyplacPieniadze(100);
+            Assert.AreEqual(100, konto1.SprawdzStanKonta);
+        }
+
+        [Test]
+        public void WyplacPieniadze_WithdrawMoneyWithInsufficientFunds()
+        {
+            var konto1 = new Bank.KontoBankowe(200);
+
+            try
+            {
+                konto1.WyplacPieniadze(300);
+            }
+            catch(Bank.WyjatekNiewystarczajaceSrodkiNaKoncie expected)
+            {
+            }
+            Assert.AreEqual(200, konto1.SprawdzStanKonta);
+        }
+
+        [Test]
+        public void TransferPieniedzy_TransferFoundsPositive_Calculated()
+        {
+            var konto1 = new Bank.KontoBankowe(300);
+            var konto2 = new Bank.KontoBankowe(500);
+
+            konto1.PrzelejKwoteNaInneKonto(konto2, 200);
+
+            Assert.AreEqual(100, konto1.SprawdzStanKonta);
+            Assert.AreEqual(700, konto2.SprawdzStanKonta);
+        }
+
+        [Test]
+        public void TransferPieniedzy_TransferFoundsWithInsufficentFounds_Calculated()
+        {
+            var konto1 = new Bank.KontoBankowe(300);
+            var konto2 = new Bank.KontoBankowe(500);
+
+            try
+            {
+                konto1.PrzelejKwoteNaInneKonto(konto2, 200);
+            }
+            catch(Bank.WyjatekNiewystarczajaceSrodkiNaKoncie expected)
+            {
+            } 
+
+            Assert.AreEqual(100, konto1.SprawdzStanKonta);
+            Assert.AreEqual(700, konto2.SprawdzStanKonta);
+        }
+    }
+}
---
+++ KontoBankowe/KontoBankowe/KontoBankowe.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace Bank
+{
+    public class KontoBankowe
+    {
+        private double _stanKonta = 0;
+
+        public KontoBankowe(double kwota)
+        {
+            this._stanKonta = kwota;
+        }
+
+        public void WyplacPieniadze(double kwota)
+        {
+            if (kwota > this._stanKonta)
+                throw new WyjatekNiewystarczajaceSrodkiNaKoncie();
+            else
+                this._stanKonta -= kwota;
+        }
+
+        public void WplacPieniadze(double kwota)
+        {
+            this._stanKonta += kwota;
+        }
+
+        public double SprawdzStanKonta
+        {
+            get { return this._stanKonta; }
+        }
+
+        public void PrzelejKwoteNaInneKonto(KontoBankowe konto, double kwota)
+        {
+            if (kwota > this._stanKonta)
+                throw new WyjatekNiewystarczajaceSrodkiNaKoncie();
+            else
+            {
+                this._stanKonta -= kwota;
+                konto._stanKonta += kwota;
+            }
+        }
+    }
+
+    public class WyjatekNiewystarczajaceSrodkiNaKoncie : ApplicationException
+    {
+
+    }
+}
